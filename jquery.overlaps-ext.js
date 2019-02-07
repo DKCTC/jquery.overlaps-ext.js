aa2ac04bf0ad0e367780df23995b03723c354b60
dkline03 <36381017+dkline03@users.noreply.github.com>
2019-02-07 15:53:16 -0500

    Update and rename jquery.overlaps.js to jquery.overlaps-ext.js

--- jquery.overlaps-ext.js
+++ jquery.overlaps-ext.js
@@ -1,6 +1,6 @@
 ﻿/*!
 	 * jquery.overlaps.js 0.0.2 - https://github.com/yckart/jquery.overlaps.js
-	 * Forked by dkline03 - https://github.com/dkline03/jquery.overlaps.js
+	 * Forked by dkline03 - https://github.com/dkline03/jquery.overlaps-ext.js
 	 *
 	 * Determine if an element overlaps another.
 	 * dkline03 - Modified to allow inner/outer height/width calculations, and modification of the dimensions by percentage
@@ -19,24 +19,36 @@
 				((/^\d+%$/).test(options.pct)) ? options.pct = parseFloat(options.pct)/100 : '';
 				//if pct is not a number, set it to 1 (or 100%)
 				(isNaN(options.pct)) ? options.pct = 1 : '';
-
-				var bounds, compare,
-					elems = {targets: [], hits:[], overlap: false};
+				
+				//the bounds of the elements in the loop
+				var bounds,
+				    //return obj
+					elems = { targets: [], hits:[], overlap: false },
+				    //calculate right and bottom of the comparison element starting with the offset
+					compare = $(obj).offset();
+				
+				//if options.inner is true, use the inner width for the overlap calculation, else use the outer width
+				if(!!options.inner){
+					compare.width = $(obj).width();
+					compare.height = $(obj).height();
+				} else {
+					compare.width = $(obj).outerWidth();
+					compare.height = $(obj).outerHeight();
+				}//if
+				
+				//calculate right and bottom, multiply by the percentage (default 1) in case you want overlap to be true only if the elements overlap by more than just the element bounds
+				//-for example, if you want the elements to overlap by 10% of the bounds before being true
+				//TODO: add ability to set the v/h alignment of the desired overlap area when options.pct is used, right now it's left/top only
+				compare.right = compare.left + (compare.width * options.pct);
+				compare.bottom = compare.top + (compare.height * options.pct);
+				
 				//go through collection
 				this.each(function() {
-					//get the bounds of this element
-					bounds = $(this).offset();
-					//calculate right and bottom
-					//-if options.inner is true, use the inner width for the overlap calculation, else use the outer width
-					//--then multiply by the percentage in case you want overlap to be true only if the elements overlap by more than just the element bounds
-					//---for example, if you want the elements to overlap by 10% of the bounds before being true
+					//calculate right and bottom bounds using same process as for the comparison element
+					bounds = $(this).offset();	
 					bounds.right = bounds.left + (((!!options.inner) ? $(this).width() : $(this).outerWidth()) * options.pct);
 					bounds.bottom = bounds.top + (((!!options.inner) ? $(this).height() : $(this).outerHeight()) * options.pct);
-					//obj is the comparison element, make into jq obj
-					compare = $(obj).offset();
-					//calculate right and bottom using same process as for the bounds
-					compare.right = compare.left + (((!!options.inner) ? $(obj).width() : $(obj).outerWidth()) * options.pct);
-					compare.bottom = compare.top + (((!!options.inner) ? $(obj).height() : $(obj).outerHeight()) * options.pct);
+					
 					//if the calculated positions overlap, set overlap to true and push elements to arrays
 					if (!(compare.right < bounds.left ||
 						  compare.left > bounds.right ||
